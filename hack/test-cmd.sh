86ca1c72a0f800c562abb6224866324da209d9f2
Peng Tao <bergwolf@gmail.com>
2018-01-26 19:56:51 +0800

    add travis ci
    Signed-off-by: Peng Tao <bergwolf@gmail.com>

---
+++ hack/test-cmd.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# This command checks that the built commands can function together for
+# simple scenarios.  It does not require Docker so it can run in travis.
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# prepare kernel and initrd
+export HYPERSTARTPATH="$GOPATH/src/github.com/hyperhq/hyperstart"
+export RUNVPATH="$GOPATH/src/github.com/hyperhq/runv"
+cd $HYPERSTARTPATH && ./autogen.sh && ./configure && make
+cp -v $HYPERSTARTPATH/build/{arch/x86_64/kernel,hyper-initrd.img} $RUNVPATH/tests/go-integration/test_data/
+
+# do runv integration-test
+cd $RUNVPATH
+hack/install-bats.sh
+make test-integration
+
